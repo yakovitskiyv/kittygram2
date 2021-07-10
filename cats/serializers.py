43a69b1ffe746dc7af3bf41eba1acc4ccba22660
Vladislav Yakovitskiy <yakovitskiyv@yandex.ru>
2021-07-10 14:25:28 +0300

    lesson 14 complite

--- cats/serializers.py
+++ cats/serializers.py
@@ -24,14 +24,35 @@ class AchievementSerializer(serializers.ModelSerializer):
 
 
 class CatSerializer(serializers.ModelSerializer):
-    achievements = AchievementSerializer(many=True, required=False)
+    achievements = AchievementSerializer(read_only=True, many=True)
     color = serializers.ChoiceField(choices=CHOICES)
+    owner = serializers.PrimaryKeyRelatedField(
+        read_only=True, default=serializers.CurrentUserDefault())
     age = serializers.SerializerMethodField()
-    
+
     class Meta:
         model = Cat
         fields = ('id', 'name', 'color', 'birth_year', 'achievements', 'owner',
-                  'age')
+                 'age')
+        
+    validators = [
+        UniqueTogetherValidator(
+            queryset=Cat.objects.all(),
+            fields=('name', 'owner')
+        )
+    ]
+
+    def validate_birth_year(self, value):
+        year = dt.date.today().year
+        if not (year - 40 < value <= year):
+            raise serializers.ValidationError('Проверьте год рождения!')
+        return value  
+
+    def validate(self, data):
+        if data['color'] == data['name']:
+            raise serializers.ValidationError(
+                'Имя не может совпадать с цветом!')
+        return data 
 
     def get_age(self, obj):
         return dt.datetime.now().year - obj.birth_year
